e777e2edefd1f6c0a1b1b02785592fbad6ac25c3
luisabackes <luisabackes2002@gmail.com>
2025-12-12 15:20:16 -0300

    feat: Cria estrutura e entidade Produto e registra módulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Produto } from './produto/entities/produto.entity';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
   imports: [
@@ -12,9 +12,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'root',
       database: 'db_delivery',
-      entities: [],
+      entities: [Produto],
       synchronize: true,
     })
+    , ProdutoModule
   ],
   controllers: [],
   providers: [],
---
+++ src/produto/entities/produto.entity.ts
@@ -0,0 +1,30 @@
+import {IsNotEmpty } from "class-validator"
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity({name: "tb_produto"})
+export class Produto {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    nome: string;
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    descricao: string;
+
+   @IsNotEmpty()
+    @Column({
+        type: 'decimal',
+        precision: 10,
+        scale: 2,
+        nullable: false
+    })
+    preco: number
+
+    @IsNotEmpty()
+    @Column({length: 5000, nullable: false})
+    foto: string
+}
