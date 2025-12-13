fac8bd90799003d82686d840dc8160abf6209791
Luis Felipe Ramires Leão <luisramires29032002@gmail.com>
2025-12-13 13:07:25 -0300

    feat : Criar o Relacionamento entre as Classes Postagem e Tema

--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Categoria } from '../../categoria/entities/categoria.entity';
 
 @Entity({ name: 'tb_produtos' })
 export class Produto {
@@ -30,4 +31,9 @@ export class Produto {
   @IsNotEmpty()
   @Column({ length: 5000, nullable: false })
   foto: string;
+
+   @ManyToOne(() => Categoria, (categoria) => categoria.produto, {
+        onDelete: "CASCADE"
+    })
+    categoria: Categoria
 }
